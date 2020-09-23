7546963a66ba9c9dfd75590686aa341da8bb1936
Lars Kellogg-Stedman <lars@redhat.com>
2020-09-23 14:31:12 -0400

    import app for access to logger

--- openshift_project.py
+++ openshift_project.py
@@ -1,8 +1,7 @@
 import json
-import logging
 import re
 
-LOG = logging.getLogger(__name__)
+from app import app
 
 
 def cnvt_project_name(project_name):
@@ -15,9 +14,9 @@ def cnvt_project_name(project_name):
 def exists_openshift_project(api, project_name):
     url = f'/apis/project.openshift.io/v1/projects/{project_name}'
     r = api.get(url)
-    LOG.debug("url: " + url)
-    LOG.debug("r: " + str(r.status_code))
-    LOG.debug("r: " + r.text)
+    app.logger.debug("url: " + url)
+    app.logger.debug("r: " + str(r.status_code))
+    app.logger.debug("r: " + r.text)
     if r.status_code == 200 or r.status_code == 201:
         return True
     return False
@@ -26,9 +25,9 @@ def exists_openshift_project(api, project_name):
 def delete_openshift_project(api, project_name):
     url = f'/apis/project.openshift.io/v1/projects/{project_name}'
     r = api.delete(url)
-    LOG.debug("url: " + url)
-    LOG.debug("r: " + str(r.status_code))
-    LOG.debug("r: " + r.text)
+    app.logger.debug("url: " + url)
+    app.logger.debug("r: " + str(r.status_code))
+    app.logger.debug("r: " + r.text)
     return r
 
 
@@ -47,8 +46,8 @@ def create_openshift_project(api, project_uuid, project_name, user_name):
         },
     }
     r = api.post(url, data=json.dumps(payload))
-    LOG.debug("url: " + url)
-    LOG.debug("payload: " + json.dumps(payload))
-    LOG.debug("r: " + str(r.status_code))
-    LOG.debug("r: " + r.text)
+    app.logger.debug("url: " + url)
+    app.logger.debug("payload: " + json.dumps(payload))
+    app.logger.debug("r: " + str(r.status_code))
+    app.logger.debug("r: " + r.text)
     return r
--- openshift_user.py
+++ openshift_user.py
@@ -1,16 +1,15 @@
 import json
-import logging
 
-LOG = logging.getLogger(__name__)
+from app import app
 
 
 def exists_openshift_user(api, user_name):
     url = f'/apis/user.openshift.io/v1/users/{user_name}'
     r = api.get(url)
 
-    LOG.warning("url: " + url)
-    LOG.warning("exists os user: " + str(r.status_code))
-    LOG.warning("exists os user: " + r.text)
+    app.logger.warning("url: " + url)
+    app.logger.warning("exists os user: " + str(r.status_code))
+    app.logger.warning("exists os user: " + r.text)
     return r.status_code == 200 or r.status_code == 201
 
 
@@ -24,17 +23,17 @@ def create_openshift_user(api, user_name, full_name):
     }
     r = api.post(url, data=json.dumps(payload))
 
-    LOG.debug("url: " + url)
-    LOG.debug("payload: " + json.dumps(payload))
-    LOG.debug("r: " + str(r.status_code))
-    LOG.debug("r: " + r.text)
+    app.logger.debug("url: " + url)
+    app.logger.debug("payload: " + json.dumps(payload))
+    app.logger.debug("r: " + str(r.status_code))
+    app.logger.debug("r: " + r.text)
     return r
 
 
 def delete_openshift_user(api, user_name):
     url = f'/apis/user.openshift.io/v1/users/{user_name}'
     r = api.delete(url)
-    LOG.debug("url: " + url)
-    LOG.debug("d os user: " + str(r.status_code))
-    LOG.debug("d os user: " + r.text)
+    app.logger.debug("url: " + url)
+    app.logger.debug("d os user: " + str(r.status_code))
+    app.logger.debug("d os user: " + r.text)
     return r

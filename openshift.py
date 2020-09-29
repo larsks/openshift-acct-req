fff92e5792320b06fcd87ec37ca7be0f28590ba7
Lars Kellogg-Stedman <lars@redhat.com>
2020-09-29 12:16:34 -0400

    fix invalid escape sequence in regular expression

--- openshift.py
+++ openshift.py
@@ -37,7 +37,7 @@ class openshift:
     def cnvt_project_name(self, project_name):
         suggested_project_name = re.sub("^[^A-Za-z0-9]+", "", project_name)
         suggested_project_name = re.sub("[^A-Za-z0-9]+$", "", suggested_project_name)
-        suggested_project_name = re.sub("[^A-Za-z0-9\-]+", "-", suggested_project_name)
+        suggested_project_name = re.sub("[^A-Za-z0-9-]+", "-", suggested_project_name)
         return suggested_project_name
 
     def get_request(self, url, debug=False):
